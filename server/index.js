9623154f7bc12e658043f166641f01a559a734fe
J-Prajwal <luckyjais058@gmail.com>
2023-03-07 21:41:55 +0530

    rollback backend to cjs

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
-dotenv.config();
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
